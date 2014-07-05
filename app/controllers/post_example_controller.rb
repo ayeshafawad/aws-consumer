95ba4196fceb0dc2179543a8ccb7a3605238780d
Ayesha Fawad <ayesha345@gmail.com>
2014-07-05 03:55:07 -0400

    Switching to puts

--- app/controllers/post_example_controller.rb
+++ app/controllers/post_example_controller.rb
@@ -3,7 +3,7 @@ class PostExampleController < ActionController::Base
   def create
     @received_headers = request.headers
     @received_body = request.body
-    Rails.logger.debug "Received request headers\n #{@received_headers.inspect} \nwith body \n#{@received_body.inspect}"
+    puts "Received request headers\n #{@received_headers.inspect} \nwith body \n#{@received_body.inspect}"
     render :confirm, status: :ok
   end
 
