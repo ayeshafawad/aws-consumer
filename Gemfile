1f733140fcd7e71cd05597b2f7ba3b73638f1a53
Ayesha Fawad <ayesha345@gmail.com>
2014-07-07 00:47:24 -0400

    Set Ruby version to 1.9.3 and bug fixes

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+ruby '1.9.3'
+
 gem 'aws-s3'
 gem 'aws-sdk'
 #gem 'paperclip'
--- app/controllers/post_example_controller.rb
+++ app/controllers/post_example_controller.rb
@@ -4,7 +4,7 @@ class PostExampleController < ActionController::Base
     received_token = received_body["Token"] unless received_body.blank?   
     topic_arn = received_body["TopicArn"] unless received_body.blank?   
     puts "Received token: #{received_token}" unless received_token.blank?
-    puts "Received topic arn: #{received_topic_arn}" unless received_topic_arn.blank?
+    puts "Received topic arn: #{topic_arn}" unless topic_arn.blank?
     confirmation_url = "https://sns.us-east-1.amazonaws.com/?Action=ConfirmSubscription&TopicArn=#{topic_arn}&Token=#{received_token}"
     puts "Send request to outbound URL: #{confirmation_url}"
     response_received = open(confirmation_url).read
