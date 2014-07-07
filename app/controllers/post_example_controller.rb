5e12dcb531c6cd7db64619386212a6d900b191e9
Ayesha Fawad <ayesha345@gmail.com>
2014-07-06 23:52:25 -0400

    Converting inbound request to hash

--- app/controllers/post_example_controller.rb
+++ app/controllers/post_example_controller.rb
@@ -2,7 +2,7 @@ class PostExampleController < ActionController::Base
 
   def create
     @received_body = JSON.parse request.body.read.to_s.strip
-    puts "Received request body:\n#{@received_body.inspect}"
+    puts "Received request body: \n#{@received_body.inspect}"
     render :confirm, status: :ok
 
   end
