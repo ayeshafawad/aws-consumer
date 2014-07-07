aacbef33087fddd31daadeb1784251b13c7947d4
Ayesha Fawad <ayesha345@gmail.com>
2014-07-07 00:38:37 -0400

    Moved parsing code into controller

--- app/controllers/post_example_controller.rb
+++ app/controllers/post_example_controller.rb
@@ -2,11 +2,12 @@ class PostExampleController < ActionController::Base
   include AwsHelper
 
   def create
-    received_token = retrieve_token(request.body.read.to_s.strip, "Token")
-    received_topic_arn = retrieve_token(request.body.read.to_s.strip, "TopicArn")
+    received_body = JSON.parse request.body.read.to_s.strip
+    received_token = received_body["Token"] unless received_body.blank?   
+    topic_arn = received_body["TopicArn"] unless received_body.blank?   
     puts "Received token: #{received_token}" unless received_token.blank?
     puts "Received topic arn: #{received_topic_arn}" unless received_topic_arn.blank?
-    confirmation_url = construct_url("ConfirmSubscription", received_topic_arn, received_token)
+    confirmation_url = "https://sns.us-east-1.amazonaws.com/?Action=ConfirmSubscription&TopicArn=#{topic_arn}&Token=#{received_token}"
     puts "Send request to outbound URL: #{confirmation_url}"
     response_received = send_confirmation_subscription(confirmation_url)
     puts "Responses received from endpoint:\n #{response_received}"
