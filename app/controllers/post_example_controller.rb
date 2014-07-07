99146ec5deaa9a400742e439e9a5f0cf18470cb5
Ayesha Fawad <ayesha345@gmail.com>
2014-07-07 01:12:16 -0400

    Switched from open-uri to net/https

--- app/controllers/post_example_controller.rb
+++ app/controllers/post_example_controller.rb
@@ -1,4 +1,7 @@
+require "net/https"
+
 class PostExampleController < ActionController::Base
+
   def create
     received_body = JSON.parse request.body.read.to_s.strip
     puts "Received body: #{received_body}" unless received_body.blank?
@@ -8,8 +11,15 @@ class PostExampleController < ActionController::Base
     puts "Received topic arn: #{topic_arn}" unless topic_arn.blank?
     confirmation_url = "https://sns.us-east-1.amazonaws.com/?Action=ConfirmSubscription&TopicArn=#{topic_arn}&Token=#{received_token}"
     puts "Send request to outbound URL: #{confirmation_url}"
-    response_received = open(confirmation_url).read
-    puts "Responses received from endpoint:\n #{response_received}"
+    uri = URI.parse confirmation_url
+    http_request = Net::HTTP::Post.new(uri.path, nil)
+    response_received = Net::HTTP.new(uri.host, uri.port).start do |http|
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.request(http_request)
+    end
+    puts "Response code received from endpoint:\n #{response_received.code}"
+    puts "Response body received from endpoint:\n #{response_received.body.presence}"
     render :confirm, status: :ok
   end  
 end
