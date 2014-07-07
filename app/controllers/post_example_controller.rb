4d0c5c9812ddd4ddcf577d4078ccae74e1209c18
Ayesha Fawad <ayesha345@gmail.com>
2014-07-07 00:57:40 -0400

    Checking for messages

--- app/controllers/post_example_controller.rb
+++ app/controllers/post_example_controller.rb
@@ -1,6 +1,7 @@
 class PostExampleController < ActionController::Base
   def create
     received_body = JSON.parse request.body.read.to_s.strip
+    puts "Received body: #{received_body}" unless received_body.blank?
     received_token = received_body["Token"] unless received_body.blank?   
     topic_arn = received_body["TopicArn"] unless received_body.blank?   
     puts "Received token: #{received_token}" unless received_token.blank?
@@ -8,7 +9,6 @@ class PostExampleController < ActionController::Base
     confirmation_url = "https://sns.us-east-1.amazonaws.com/?Action=ConfirmSubscription&TopicArn=#{topic_arn}&Token=#{received_token}"
     puts "Send request to outbound URL: #{confirmation_url}"
     response_received = open(confirmation_url).read
-    response_received
     puts "Responses received from endpoint:\n #{response_received}"
     render :confirm, status: :ok
   end  
