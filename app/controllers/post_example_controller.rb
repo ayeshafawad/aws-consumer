9aa8fbf8cb0fd6294deb2a75fb9e1cbd353e1641
Ayesha Fawad <ayesha345@gmail.com>
2014-07-07 04:29:12 -0400

    Switched from open-uri to net/https

--- app/controllers/post_example_controller.rb
+++ app/controllers/post_example_controller.rb
@@ -19,9 +19,9 @@ class PostExampleController < ActionController::Base
     uri.port = Net::HTTP.https_default_port()
     puts "URI port: #{uri.port}"
     puts "URI host: #{uri.host}"
-    http = Net::HTTP.new(uri.host, uri.port)
-    response = http.request(http_request)
-    puts "Response:\n #{response}"
+    http = Net::HTTP.new(uri.host, uri.port).start 
+    #response = http.request(http_request)
+    #puts "Response:\n #{response}"
 
     # response_received = Net::HTTP.new(uri.host, uri.port).start 
     # do |http|
