fe465ed7f3facee59d1bd632fffcb07acaf5c406
Ayesha Fawad <ayesha345@gmail.com>
2014-07-07 17:11:07 -0400

    notifications

--- app/controllers/post_example_controller.rb
+++ app/controllers/post_example_controller.rb
@@ -16,15 +16,15 @@ class PostExampleController < ActionController::Base
     #      puts "Message received from AWS Topic. Id:#{message_Id}, Subject:#{message_subject}, Message:#{message_received}"
     #    end
 
-    # if received_message_type == "Notification"
-    #     message_subject = received_body["Subject"]
-    #     message_received = received_body["Message"]
-    #     message_id = received_body["MessageId"]
-    #     puts "Message received from AWS Topic. Id:#{message_id}, Subject:#{message_subject}, Message:#{message_received}"
-    #    unless message_subject.blank? && message_received.blank? && message_id.blank?
-    #      puts "Message received from AWS Topic. Id:#{message_Id}, Subject:#{message_subject}, Message:#{message_received}"
-    #    end
-    #  end
+    if received_message_type == "Notification"
+        message_subject = received_body["Subject"]
+        message_received = received_body["Message"]
+        message_id = received_body["MessageId"]
+        puts "Message received from AWS Topic. Id:#{message_id}, Subject:#{message_subject}, Message:#{message_received}"
+       unless message_subject.blank? && message_received.blank? && message_id.blank?
+         puts "Message received from AWS Topic. Id:#{message_Id}, Subject:#{message_subject}, Message:#{message_received}"
+       end
+     end
 
     received_token = received_body["Token"] unless received_body.blank?   
     topic_arn = received_body["TopicArn"] unless received_body.blank?   
@@ -46,7 +46,7 @@ class PostExampleController < ActionController::Base
     #response = http.request(Net::HTTP::Get.new(uri.request_uri))
     
 
-    response = http.request(http_request)
+    #response = http.request(http_request)
     puts "Response from AWS:\n #{response}"
 
     # response_received = Net::HTTP.new(uri.host, uri.port).start 
